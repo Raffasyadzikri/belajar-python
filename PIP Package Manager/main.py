74b2dcc02c5db5164c984e20dc91b1a8bb1b57bc
Raffasya Dzikri <raffasya.dzikri@student.alhazen.academy>
2023-11-10 17:16:51 +0700

    string color and emoji

---
+++ PIP Package Manager/main.py
@@ -0,0 +1,10 @@
+from stringcolor import*
+from emoji import *
+
+print(cs("Raffasya Dzikri","blue"))
+print(bold("Maa syaa Allah"))
+print(cs("Raffasya Dzikri","blue").underline())
+print(cs("Rafasya Dzikri", "blue", "white"))
+
+print(emojize(":cold_face:"))
+print(emojize(":thumbs_up:"))
